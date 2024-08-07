bb68fa1e0326eb7837b6bc7d1923c763d4bfd9d2
AlamStr <satriaalm9@gmail.com>
2024-08-07 21:05:54 +0700

    baru

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@ const navbarnav = document.querySelector(".navbar-nav");
 document.querySelector("#Hamburger-menu").onclick = () => {
   navbarnav.classList.toggle("active");
 };
+
+// Klik di luar sidebar untuk menghilangkan  nav
+const Hamburger = document.querySelector("#Hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!Hamburger.contains(e.target) && !navbarnav.contains(e.target)) {
+    navbarnav.classList.remove("active");
+  }
+});
